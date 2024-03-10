01ddddfe55e6de524c73d70ce9a475f35fefb6c9
KatliKoma <katlegokoma1@gmail.com>
2024-03-10 12:32:04 +0200

    pycodestyle

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,1 @@
-# Import all the classes
-from models.base_model import BaseModel
 
-# should be imported when using 'from models import *'
-__all__ = [
-    'BaseModel',
-    # Add more class names here if needed
-]
