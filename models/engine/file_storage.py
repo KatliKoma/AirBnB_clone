b460bf705ac80c39f21ad72d132c6cc77fdbb0ef
octaviav <octavia7104@gmail.com>
2024-03-10 07:58:29 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,90 +1,67 @@
 #!/usr/bin/python3
-"""Module containing the FileStorage class."""
+"""
+Module for managing serialization and deserialization of data
+"""
 import json
 import os
-import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
+
 
 class FileStorage:
-
-    """Class where data is stored and can be retreived"""
-    __objects = {}
+    """
+    FileStorage class manages storage, serialization, and deserialization of data
+    """
     __file_path = "file.json"
 
-    def all(self):
-        """returns the dictionary __objects"""
-        return FileStorage.__objects
-
-    def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        key in "{}.{}".format(type(obj).__name__,obj.id)
-        FileStorage.__objects[key] = obj
+    __objects = {}
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-            json.dump(d, f)
+        """
+        Serializes the objects dictionary into JSON format and saves it to the specified file.
+        """
+        all_objects = FileStorage.__objects
+
+        object_dict = {}
+
+        for obj_key, obj_value in all_objects.items():
+            object_dict[obj_key] = obj_value.to_dict()
+
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(object_dict, file)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
+        """
+        Deserializes the JSON file and restores the objects dictionary.
+        """
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                try:
+                    object_dict = json.load(file)
 
-    def classes(self):
-        """returns a dictionarr of classes and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-        from models.place import Place
-            
-            classes = {"BaseModel": BaseModel,
-                "User": User,
-                "State": State,
-                "City": City,
-                "Anemity": Amenity,
-                "Place": Place,
-                "Review": Review,
-            return classes
+                    for key, value in object_dict.items():
+                        class_name, obj_id = key.split('.')
+                        cls = eval(class_name)
+                        instance = cls(**value)
+                        FileStorage.__objects[key] = instance
+                except Exception:
+                    pass
 
-    def attributes(self):
-        """Returns attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attributes
+    def new(self, obj):
+        """
+        Adds a new object to the objects dictionary.
+        """
+        class_name = obj.__class__.__name__
+        key = "{}.{}".format(class_name, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def all(self):
+        """
+        Returns the objects dictionary.
+        """
+        return FileStorage.__objects
