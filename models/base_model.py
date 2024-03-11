7225c223dcd1684cafa59cbbf5348094ff1214c4
KatliKoma <katlegokoma1@gmail.com>
2024-03-11 10:14:41 +0200

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,73 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
+"""
+Module: base_model
+Contains a class that defines common attributes and methods for other classes
+"""
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """Represents the base model of the HBnB project."""
+    ''' Base class for other classes '''
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel instance.
-
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, time_format)
-                else:
-                    self.__dict__[key] = value
+        '''
+        Initializes the object's attributes.
+        '''
+        if kwargs:
+            date_format = '%Y-%m-%dT%H:%M:%S.%f'
+            kwargs_copy = kwargs.copy()
+            del kwargs_copy["__class__"]
+            for key in kwargs_copy:
+                if ("created_at" == key or "updated_at" == key):
+                    kwargs_copy[key] = datetime.strptime(kwargs_copy[key], date_format)
+            self.__dict__ = kwargs_copy
         else:
-            models.storage.new(self)
-
-    def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
-        models.storage.save()
-
-    def to_dict(self):
-        """Return the dictionary representation of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        result_dict = self.__dict__.copy()
-        result_dict["created_at"] = self.created_at.isoformat()
-        result_dict["updated_at"] = self.updated_at.isoformat()
-        result_dict["__class__"] = self.__class__.__name__
-        return result_dict
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        """Return the string representation of the BaseModel instance."""
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        '''
+        Prints in "[<class name>] (<self.id>) <self.__dict__>" format
+        '''
+        return ('[{}] ({}) {}'.format(
+            self.__class__.__name__,
+            self.id,
+            self.__class__.__dict__))
+
+    def save(self):
+        '''
+        Updates the public instance attribute updated_at
+        with the current datetime
+        '''
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        '''
+        Returns a dictionary containing all keys and values
+        of __dict__ of the instance
+        '''
+        dictionary = {}
+        dictionary["__class__"] = self.__class__.__name__
+        for key, value in self.__dict__.items():
+            if isinstance(value, (datetime, )):
+                dictionary[key] = value.isoformat()
+            else:
+                dictionary[key] = value
+        return dictionary
+
+    def to_json(self):
+        '''
+        Returns a JSON containing all keys and values
+        of __dict__ of the instance
+        '''
+        my_json = self.__dict__.copy()
+        my_json.update({'created_at': self.created_at.strftime(self.date_format)})
+        my_json.update({'__class__': str(self.__class__.__name__)})
+        if hasattr(self, 'updated_at'):
+            my_json.update({'updated_at': self.updated_at.strftime(self.date_format)})
+        return my_json
