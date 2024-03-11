664d67efb867d3de75d3f3af3b004c5557b8872a
KatliKoma <katlegokoma1@gmail.com>
2024-03-11 10:48:05 +0200

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,76 +6,56 @@ import uuid
 from datetime import datetime
 import models
 
-    """
-        Initializes a new instance of the BaseModel class.
-        If 'kwargs' is provided, it populates the
-        instance with the key-value pairs in 'kwargs',
-        converting 'created_at' and 'updated_at'
-        to datetime objects if necessary.
-        """
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        """
+        Initializes a new instance of the BaseModel class.
+        If 'kwargs' is provided, it populates the instance with the key-value pairs in 'kwargs',
+        converting 'created_at' and 'updated_at' to datetime objects if necessary.
+        Otherwise, it sets default values for 'id', 'created_at', and 'updated_at'.
+        """
+
+def __init__(self, *args, **kwargs) -> None:
+
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-        
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, time_format))
+                    pass
+                elif key != "created_at" and key != "updated_at":
+                    self.__dict__[key] = value
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[key] = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            models.storage.new(self)
 
-        models.storage.new(self)
+    def __str__(self) -> str:
+        """[Changing the str method expected output to :
+        [<class name>] (<self.id>) <self.__dict__>]
+
+        Returns:
+            str: [description with the information changed]
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """[Function that updates the update_date]
         """
-
-        """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
+        """[Function that returns an specific information about the class
+        in a dict]
+
+        Returns:
+            [dict]: [The atttributes with the format required]
         """
-
-        """
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
-
-        return inst_dict
-
-    def __str__(self):
-        """
-
-        """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My_First_Model"
-    my_model.my_number = 89
-    print(my_model.id)
-    print(my_model)
-    print(type(my_model.created_at))
-    print("--")
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
+        copy = self.__dict__.copy()
+        copy["__class__"] = self.__class__.__name__
+        copy["created_at"] = self.created_at.isoformat()
+        copy["updated_at"] = self.updated_at.isoformat()
+        return copy
