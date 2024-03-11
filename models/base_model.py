3fea9789e867435810c1e9de9edae0fda8c448d0
KatliKoma <katlegokoma1@gmail.com>
2024-03-11 13:25:56 +0200

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     Defines all common attributes/methods for other classes.
@@ -29,13 +30,15 @@ class BaseModel:
 
     def save(self):
         """
-        Updates the public instance attribute updated_at with the current datetime.
+        Updates the public instance attribute
+        updated_at with the current datetime.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance.
+        Returns a dictionary containing all
+        keys/values of __dict__ of the instance.
         """
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
