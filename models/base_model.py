bd05119646985951682754e04dc4191c4a41027b
KatliKoma <katlegokoma1@gmail.com>
2024-03-11 10:57:01 +0200

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,64 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class for HBnB (Holberton BnB) project."""
-
+"""
+0x00. AirBnB clone - The console
+Base_model module
+"""
 import uuid
-from datetime import datetime
+import datetime
 import models
 
 class BaseModel:
-    """Represents a base model with common attributes and methods for HBnB project."""
+    """
+    Defines all common attributes/methods for other classes
+    """
 
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new BaseModel instance.
-        
-        Attributes are set with the current datetime if not provided via kwargs.
-        The id attribute is assigned a unique UUID string value.
-        If kwargs is provided, it overrides default attributes except for __class__.
-        """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-
-        if kwargs:
+        """initialize variables and methods"""
+        if kwargs is not None and len(kwargs) > 0:
             for key, value in kwargs.items():
-                if key == "__class__":
+                if key == '__class__':
                     continue
-                elif key in ("created_at", "updated_at"):
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                else:
-                    setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                try:
+                    if value.isdigit():
+                        value = int(value)
+                    elif value.replace('.', '', 1).isdigit():
+                        value = float(value)
+                except AttributeError:
+                    pass
+                setattr(self, key, value)
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
             models.storage.new(self)
 
-    def __str__(self):
-        """Returns a string representation of the BaseModel instance."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
     def save(self):
-        """Updates the updated_at attribute to the current datetime."""
-        self.updated_at = datetime.utcnow()
+        """
+        method that updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of the instance.
-        
-        Adds the class name with the key '__class__'.
-        Formats created_at and updated_at to ISO format strings.
+        method that returns a dictionary containing all keys/values of
+        __dict__ of the BaseModel class object/instance, as well as
+        a new '__class__' attribute and its value (class name of the object);
+        this method also converts the 'created_at' and 'updated_at' attributes
+        to string object in ISO format (%Y-%m-%dT%H:%M:%S.%f)
         """
-        dict_rep = self.__dict__.copy()
-        dict_rep["__class__"] = self.__class__.__name__
-        dict_rep["created_at"] = self.created_at.isoformat()
-        dict_rep["updated_at"] = self.updated_at.isoformat()
-        return dict_rep
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
+
+    def __str__(self):
+        """
+        method that returns a string representation of an object/instance
+        in this format: [<class name>] (<self.id>) <self.__dict__>
+        """
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
