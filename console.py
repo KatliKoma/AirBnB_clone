a27bcd882368bafa9a0b049abf44d85ed0a69aff
KatliKoma <katlegokoma1@gmail.com>
2024-03-11 18:34:12 +0200

    console

--- console.py
+++ console.py
@@ -1,10 +1,39 @@
 #!/usr/bin/python3
+"""Defines the HBnB console."""
 import cmd
+import re
+from shlex import split
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+def parse(arg):
+    """Parse command arguments."""
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 class HBNBCommand(cmd.Cmd):
-    """A command interpreter for AirBnB clone."""
-
-    prompt = '(hbnb) ' # Custom prompt
+    """Defines the HolbertonBnB command interpreter."""
+    prompt = "(hbnb) "
+    __classes = {"BaseModel", "User", "State", "City", "Place", "Amenity", "Review"}
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
@@ -40,5 +69,7 @@ class HBNBCommand(cmd.Cmd):
             print("=========================================")
             cmd.Cmd.do_help(self, arg)
 
-if __name__ == '__main__':
+    # Additional command methods can be added here
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
