85b301cfb134e90a7648ac36833d226afb5da6df
KatliKoma <katlegokoma1@gmail.com>
2024-03-13 01:51:51 +0200

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Contains the BaseModel class
+This module contains the BaseModel class
+which is the base class for all other classes in this project.
 """
 
 import uuid
@@ -10,12 +11,15 @@ import models
 
 class BaseModel:
     """
-    The BaseModel class from which future classes will be derived
+    Defines all common attributes/methods for other classes.
     """
 
     def __init__(self, *args, **kwargs):
         """
-        Initialization of the base model
+        Initializes a new instance of BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
         if kwargs:
             for key, value in kwargs.items():
@@ -26,26 +30,26 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+        models.storage.new(self)
 
     def __str__(self):
         """
-        String representation of the BaseModel class
+        Returns a string representation of the BaseModel instance.
         """
         return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+                self.__class__.__name__, self.id, self.__dict__
+                )
 
     def save(self):
         """
-        Updates the attribute 'updated_at' with the current datetime
+        Updates 'updated_at' with the current datetime.
         """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all
-        keys/values of __dict__ of the instance
+        Returns a dictionary containing all keys/values of the instance.
         """
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import unittest
-import models
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
+    """
+    Test the BaseModel class.
+    """
 
     def test_instance_creation(self):
         """Test the creation of a BaseModel instance and its attributes."""
@@ -12,6 +15,31 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(hasattr(instance, "id"))
         self.assertTrue(hasattr(instance, "created_at"))
         self.assertTrue(hasattr(instance, "updated_at"))
+        self.assertIsInstance(instance.created_at, datetime)
+        self.assertIsInstance(instance.updated_at, datetime)
+        self.assertEqual(str, type(instance.id))
+
+    def test_str_method(self):
+        """Test that the str method has the correct output."""
+        instance = BaseModel()
+        expected = "[BaseModel] ({}) {}".format(instance.id, instance.__dict__)
+        self.assertEqual(expected, str(instance))
+
+    def test_save_method(self):
+        """Test the save method updates 'updated_at' and persists changes."""
+        instance = BaseModel()
+        old_updated_at = instance.updated_at
+        instance.save()
+        self.assertNotEqual(old_updated_at, instance.updated_at)
+
+    def test_to_dict_method(self):
+        """Test the to_dict method returns the correct dictionary."""
+        instance = BaseModel()
+        instance_dict = instance.to_dict()
+        self.assertEqual(instance_dict['__class__'], 'BaseModel')
+        self.assertEqual(instance_dict['id'], instance.id)
+        self.assertIsInstance(instance_dict['created_at'], str)
+        self.assertIsInstance(instance_dict['updated_at'], str)
 
 
 if __name__ == "__main__":
