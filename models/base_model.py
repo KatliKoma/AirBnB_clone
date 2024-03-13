236f3840d1feca3044938caf72564053b9db4718
KatliKoma <katlegokoma1@gmail.com>
2024-03-13 02:26:13 +0200

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -21,18 +21,16 @@ class BaseModel:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
-        def __init__(self, *args, **kwargs):
-            if not kwargs:
-                self.id = str(uuid.uuid4())
-                self.created_at = self.updated_at = datetime.now()
-            else:
-                for key, value in kwargs.items():
-                    if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(
-                                value, '%Y-%m-%dT%H:%M:%S.%f'
-                                )
-                    if key != '__class__':
-                        setattr(self, key, value)
+                if kwargs:
+                    for key, value in kwargs.items():
+                        if key == 'created_at' or key == 'updated_at':
+                            value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        if key != '__class__':
+                            setattr(self, key, value)
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = self.updated_at = datetime.now()
+                models.storage.new(self)
 
     def __str__(self):
         """
