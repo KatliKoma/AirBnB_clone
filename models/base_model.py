2dad54ec1a1b9cc020d62994d6f80fa3c812d637
KatliKoma <katlegokoma1@gmail.com>
2024-03-13 02:27:53 +0200

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -21,24 +21,22 @@ class BaseModel:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
-                if kwargs:
-                    for key, value in kwargs.items():
-                        if key == 'created_at' or key == 'updated_at':
-                            value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                        if key != '__class__':
-                            setattr(self, key, value)
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = self.updated_at = datetime.now()
-                models.storage.new(self)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+        models.storage.new(self)
 
     def __str__(self):
         """
         Returns a string representation of the BaseModel instance.
         """
-        return "[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, self.__dict__
-                )
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
